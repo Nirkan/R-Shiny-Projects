79ee26075c5c6dff87fa1b6f660df89d1eed6963
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-06-30 14:34:20 +0200

    Ozone histogram app

--- Shiny-Airquality/Shiny-airquality.R
+++ Shiny-Airquality/Shiny-airquality.R
@@ -1,4 +1,63 @@
-# library and dataset
+## library and dataset
 
 library(shiny)
 data("airquality")
+
+
+## Defining UI for the app to draw a histogram
+
+ui <- fluidPage(
+  
+  # App title
+  titlePanel("Ozone level"),
+  
+  # Sidebar layout with input and output definitions
+  sidebarLayout(
+    
+    # Sidebar panel for inputs
+    sidebarPanel(
+      
+      #Input : Slider for the number of bins
+      sliderInput(
+        inputId = "bins",
+        label = "Number of bins:",
+        min = 1,
+        max = 75,
+        value = 30),
+    
+  ),
+    
+    # Main panel for displaying outputs
+    mainPanel(
+      
+      # Output: Histogram
+      plotOutput(outputId = "distPlot"),
+    ),
+  
+    
+  )
+  
+)
+  
+
+
+## Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    
+    x <- airquality$Ozone
+    x <- na.omit(x)
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    hist(x, breaks = bins, col = "yellow3", border = "black",
+                xlab = "Ozone level",
+                main = "Histogram of Ozone level")
+  })
+
+}
+
+
+
+## Create a Shiny app
+shinyApp(ui = ui, server = server)
